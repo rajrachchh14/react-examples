9d74af27d65da3860f944ace1ef099735d730011
rajrachchh14 <rajrachchh14@gmail.com>
2021-08-31 23:42:15 +0530

    Top-bar

--- src/Pages/Topbar.js
+++ src/Pages/Topbar.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap'; //  NavLink,
 import { Link } from 'react-router-dom';
 function Topbar() {
   return (
+    // <>
+    //   <Link to="/">Home Page</Link>
+
+    //   <Link to="/list">List</Link>
+
+    //   <Link to="/detail">Detail</Link>
+
+    //   <Link to="/search">Searchs</Link>
+
+    //   <Link to="/create">Create</Link>
+    // </>
+
     <>
-      <Link to="/">Home Page</Link>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Webcode</Navbar.Brand>
 
-      <Link to="/list">List</Link>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">
+            Home Page
+          </Nav.Link>
 
-      <Link to="/detail">Detail</Link>
+          <Nav.Link as={Link} to="/list">
+            List
+          </Nav.Link>
 
-      <Link to="/search">Searchs</Link>
+          <Nav.Link as={Link} to="/detail">
+            Detail
+          </Nav.Link>
 
-      <Link to="/create">Create</Link>
+          <Nav.Link as={Link} to="/search">
+            Searchs
+          </Nav.Link>
+
+          <Nav.Link as={Link} to="/create">
+            Create
+          </Nav.Link>
+        </Nav>
+      </Navbar>
     </>
   );
 }
